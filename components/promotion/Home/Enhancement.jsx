fb2e1bee0b97ceeef0b69bfb00dbec6146d6d014
naima-uddin <toktikproject@gmail.com>
2026-02-18 15:41:37 +0600

    Update Enhancement.jsx

--- components/promotion/Home/Enhancement.jsx
+++ components/promotion/Home/Enhancement.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Enhancement = () => {
   return (
-    <section className="bg-gray-10/20 pt-10 pb-6  md:pb-8  lg:pb-6 mb-10">
+    <section className="bg-gray-10/10 pt-10 pb-6  md:pb-8  lg:pb-6 mb-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-5 gap-24 items-center lg:items-end">
         {/* Left - mockup image */}
         <div className="flex justify-start items-end relative lg:col-span-2 lg:self-end">
